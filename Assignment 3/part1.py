0498e006ea6f07dbceac68827689290f970a671f
Jack Yuan <kjackyuan@gmail.com>
2018-04-05 18:40:12 -0400

    Add files via upload

--- Assignment 3/part1.py
+++ Assignment 3/part1.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+import pdb
+import scipy.misc
 
 with np.load("notMNIST.npz") as data:
     Data, Target = data ["images"], data["labels"]
@@ -13,8 +15,14 @@ with np.load("notMNIST.npz") as data:
     validData, validTarget = Data[15000:16000], Target[15000:16000]
     testData, testTarget = Data[16000:], Target[16000:]
 
+trainData = np.concatenate([trainData[trainTarget == 0], trainData[trainTarget == 1]])
+trainTarget = np.concatenate([trainTarget[trainTarget == 0], trainTarget[trainTarget == 1]])
 
-n_classes = 10
+testData = np.concatenate([testData[testTarget == 0], testData[testTarget == 1]])
+testTarget = np.concatenate([testTarget[testTarget == 0], testTarget[testTarget == 1]])
+
+
+n_classes = 2
 num_feature = 28*28
 train_size = len(trainData)
 
@@ -37,32 +45,50 @@ def build_layer(prev_layer, num_node):
 x = tf.placeholder("float", shape=[None, num_feature])
 y_true = tf.placeholder("float", shape=[None, n_classes])
 
-keep_rate = 0.7
+keep_rate = 0.6
 
-layer1 = tf.nn.dropout(tf.nn.relu(build_layer(x, 500)), keep_rate)
+layer1 = tf.nn.relu(build_layer(x, 100))
 
-layer2 = tf.nn.dropout(tf.nn.relu(build_layer(layer1, 500)), keep_rate)
-
-layer3 = tf.nn.dropout(tf.nn.relu(build_layer(layer2, 500)), keep_rate)
-
-assert False
-
-y = build_layer(layer3, n_classes) #output layer
+y = build_layer(layer1, n_classes) #output layer
 
 all_weight = tf.concat(layer_weight, 0)
 
 weight_decay = 0.0003
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y)) + 0.5*weight_decay*tf.reduce_sum(tf.square(all_weight))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y))# + 0.5*weight_decay*tf.reduce_sum(tf.square(all_weight))
 
 #learning rate = 0.001
-train_step = tf.train.AdamOptimizer().minimize(loss)
+train_step = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
 
 batch_size = 500
-epoch_ratio = int(train_size/batch_size)
-iteration = 15000
+iteration = 10*batch_size
 
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_true, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+def normalize(mat):
+    mat_min = np.min(mat)
+    mat = mat - mat_min
+    mat_max = np.max(mat)
+    mat = mat/mat_max
+    mat = mat*255.0
+    return mat
+
+def visualize(perc):
+    a = s.run(tf.reshape(tf.transpose(tf.reshape(layer_weight[0], [784, 100])), [100, 28, 28]))
+    final_img = None
+
+    for col in range(10):
+        col_img = normalize(a[col*10])
+        for row in range(1, 10):
+            col_img = np.concatenate([col_img, np.zeros((1, 28)), normalize(a[col*10+row])])
+        if final_img is None:
+            final_img = np.transpose(col_img)
+        else:
+            col_size = len(col_img)
+            col_img = np.transpose(col_img)
+            final_img = np.concatenate([final_img, np.zeros((1, col_size)), col_img])
+
+    scipy.misc.imsave('%s.jpg'%perc, final_img)
 
 
 with tf.Session() as s:
@@ -76,13 +102,16 @@ with tf.Session() as s:
     
         train_step.run(feed_dict={x: batch_data, y_true: batch_target})
     
-        if step%(epoch_ratio)==0:
+        if step%(batch_size)==0:
             randIndx = np.arange(len(trainData))
             np.random.shuffle(randIndx)
             trainData, trainTarget = trainData[randIndx], trainTarget[randIndx]
             trainTarget_oh = s.run(tf.one_hot(trainTarget, n_classes))
         
-        if step%(epoch_ratio) == 0:
+        if step%(batch_size) == 0:
             print s.run(loss, feed_dict={x: trainData, y_true: trainTarget_oh})
             print accuracy.eval({x: testData, y_true: testTarget_oh})
             print ''
+
+        if step%batch_size == 0:
+            visualize(step/batch_size)
